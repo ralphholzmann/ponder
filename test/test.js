09027a7252ede7e4d7165954814fd4da93d29606
Ralph Holzmann <ralph@holzmann.io>
2017-09-17 23:01:21 -0500

    Characters dont have emails

--- test/test.js
+++ test/test.js
@@ -4,12 +4,14 @@ import { Database, Model } from '../src';
 class Character extends Model {
   static schema = {
     name: String,
-    email: String
+    age: Number,
+    magicType: String,
+    weaponType: String
   }
 
   static indexes = {
     name: true,
-    name_email: ['name', 'email']
+    magicType_weaponType: ['magicType', 'weaponType']
   };
 
   static relations = [{
@@ -18,9 +20,18 @@ class Character extends Model {
     primaryKey: 'id',
     foreignKey: 'characterId'
   }];
-};
+}
+
+class Weapon extends Model {
+  static schema = {
+    name: String,
+    type: String,
+    attack: Number
+  }
+}
 
 Database.register(Character);
+Database.register(Weapon);
 
 test.before(async () => {
   Database.config({
@@ -32,7 +43,9 @@ test.before(async () => {
 test('Saving a new model instance adds an id to the instance', async (t) => {
   const user = new Character({
     name: 'Crono',
-    email: 'crono@theendoftime.com'
+    age: 17,
+    magicType: 'light',
+    weaponType: 'katana'
   });
   const returnedUser = await user.save();
 
@@ -42,25 +55,25 @@ test('Saving a new model instance adds an id to the instance', async (t) => {
 
 test('Queries return instances of models', async (t) => {
   const [user] = await Character.filter({
-    email: 'crono@theendoftime.com'
+    age: 17
   }).run();
 
   t.true(user instanceof Character);
   t.is(user.name, 'Crono');
-  t.is(user.email, 'crono@theendoftime.com');
+  t.is(user.age, 17);
 });
 
 test('Saving an existing model updates correctly', async (t) => {
   const [user] = await Character.filter({
-    email: 'crono@theendoftime.com'
+    weaponType: 'katana'
   }).run();
-  user.email = 'frog@theendoftime.com';
+  user.age += 1;
   await user.save();
 
   const [updatedUser] = await Character.filter({
-    email: 'frog@theendoftime.com'
+    weaponType: 'katana'
   }).run();
-  t.is(updatedUser.email, 'frog@theendoftime.com');
+  t.is(updatedUser.age, 18);
 });
 
 test('simple indexes are created successfully', async (t) => {
@@ -69,16 +82,14 @@ test('simple indexes are created successfully', async (t) => {
   }).run();
   t.true(user instanceof Character);
   t.is(user.name, 'Crono');
-  t.is(user.email, 'frog@theendoftime.com');
 });
 
 test('compound indexes are created successfully', async (t) => {
-  const [user] = await Character.getAll(['Crono', 'frog@theendoftime.com'], {
-    index: 'name_email'
+  const [user] = await Character.getAll(['light', 'katana'], {
+    index: 'magicType_weaponType'
   }).run();
   t.true(user instanceof Character);
   t.is(user.name, 'Crono');
-  t.is(user.email, 'frog@theendoftime.com');
 });
 
 test('Changefeeds return instances of models', async (t) => {
@@ -91,7 +102,9 @@ test('Changefeeds return instances of models', async (t) => {
 
     const user = new Character({
       name: 'Marle',
-      email: 'marle@thendoftime.com'
+      age: 16,
+      magicType: 'ice',
+      weaponType: 'crossbow'
     });
     user.save();
   });
@@ -103,22 +116,24 @@ test('Changefeeds `diff` correctly', async (t) => {
   const cursor = await Character.changes().run();
   await new Promise(async (resolve) => {
     const user = new Character({
-      name: 'Marle',
-      email: 'marle@thendoftime.com'
+      name: 'Frog',
+      age: 38,
+      magicType: 'water',
+      weaponType: 'boardsword'
     });
 
     cursor.each((change) => {
       if (count === 1) {
         const diff = change.diff();
         t.is(user.id, diff.id);
-        t.is(user.email, diff.email);
+        t.is(user.age, diff.age);
         resolve();
       }
       count += 1;
     });
 
     await user.save();
-    user.email = 'marle@fiendlordskeep.com';
+    user.age += 1;
     await user.save();
   });
   await cursor.close();
