bce88a8bebecf8b749254a5c88378f6ef392d8ab
Ralph Holzmann <ralph@holzmann.io>
2017-10-13 11:26:55 -0500

    Private property mixin

---
+++ src/mixins/PrivateMixin.js
@@ -0,0 +1,28 @@
+const NAMESPACE = Symbol('ProtectedMixin');
+
+module.exports = (superclass) => {
+  class PrivateMixin extends superclass {
+    setContext(context) {
+      this[NAMESPACE] = {
+        context
+      };
+    }
+
+    toJSON() {
+      const { context } = this[NAMESPACE];
+      return Object.keys(this.constructor.schema).reduce((json, property) => {
+        const config = this.constructor.schema[property];
+        if (config.type) {
+          if (config.private === false || (typeof config.private === 'function' && config.private(this, context))) {
+            json[property] = this[property];
+          }
+        } else {
+          json[property] = this[property];
+        }
+        return json;
+      }, {});
+    }
+  }
+
+  return PrivateMixin;
+};
---
+++ test/mixins/Private.js
@@ -0,0 +1,62 @@
+import test from 'ava';
+import { Database, Model } from '../../src';
+import PrivateMixin from '../../src/mixins/PrivateMixin.js';
+
+class AppUser extends Model.with(PrivateMixin) {
+  static schema = {
+    name: String,
+    email: {
+      type: String,
+      private: (instance, context) => context.user.id === instance.id
+    },
+    ipAddress: {
+      type: String,
+      private: true
+    }
+  }
+};
+
+test.before(async () => {
+  Database.config({
+    db: 'test_db'
+  });
+  await Database.connect();
+});
+
+Database.register(AppUser);
+
+test('Private properties are hidden from payloads correctly', async (t) => {
+  const appUser1 = new AppUser({
+    name: 'Ralph',
+    email: 'ralph@example.com',
+    ipAddress: '127.0.0.1'
+  });
+
+  const appUser2 = new AppUser({
+    name: 'Jackson',
+    email: 'jackson@example.com',
+    ipAddress: '127.0.0.2'
+  });
+
+  const req1 = {
+    user: appUser1
+  };
+
+  const req2 = {
+    user: appUser2
+  };
+  await appUser1.save();
+  await appUser2.save();
+
+  appUser1.setContext(req1);
+  const payload1 = JSON.parse(JSON.stringify(appUser1));
+  t.is(payload1.name, 'Ralph');
+  t.is(payload1.email, 'ralph@example.com');
+  t.falsy(payload1.ipAddress);
+
+  appUser1.setContext(req2);
+  const payload2 = JSON.parse(JSON.stringify(appUser1));
+  t.is(payload2.name, 'Ralph');
+  t.falsy(payload2.email);
+  t.falsy(payload2.ipAddress);
+});
